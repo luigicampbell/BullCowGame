54e288c67a68ed5bff991de83ba2b138cd05ae6b
Luigi Campbell <luigi.campbell@outlook.com>
2018-09-23 10:02:27 -0700

    basic user input

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "Welcome to Bulls and Cows\n";
+	constexpr int WORD_LENGTH = 5;
+	string Guess = "";
+	cout 
+		<< "Welcome to Bulls and Cows, a fun word game.\n"
+		<< "Try to guess the "
+		<< WORD_LENGTH
+		<< " letter isogram!\n";
+
+	// get player guess
+	cin >> Guess;
+
+	// repeat player guess
+	cout 
+		<<"You guessed: "
+		<< Guess 
+		<< endl;
+
 	system("pause");
 	return 0;
 }
