a994350620f86f5938c0ddda111193d785197139
Luigi Campbell <luigi.campbell@outlook.com>
2018-09-23 19:35:29 -0700

    made definition files

---
+++ BullCowGame/FBullCowGame.cpp
@@ -0,0 +1,26 @@
+#include "FBullCowGame.h"
+
+void FBullCowGame::Reset()
+{
+
+}
+
+int FBullCowGame::GetMaxTries()
+{
+
+}
+
+int FBullCowGame::GetCurrentTry()
+{
+
+}
+
+bool FBullCowGame::IsGameWon()
+{
+
+}
+
+bool CheckGuessValidity(std::string)
+{
+
+}
